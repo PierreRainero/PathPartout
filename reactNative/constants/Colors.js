be86cd82cdd278a7960e085e5656180a3046a70c
Gregory Merlet <gregory.merlet@outlook.fr>
2018-10-05 11:27:47 +0200

    Changing few things on the react native app

--- reactNative/constants/Colors.js
+++ reactNative/constants/Colors.js
@@ -1,14 +1,28 @@
-const tintColor = '#2f95dc';
+const primaryColor = '#32cd32';
+const primaryColorLight = '#73ff65';
+const primaryColorDark = '#009b00';
+const primaryColorText = '#ffffff';
+const secondaryColor = '#bdbdbd';
+const secondaryColorLight = '#efefef';
+const secondaryColorDark = '#8d8d8d';
+const secondaryColorText = '#000000';
 
 export default {
-  tintColor,
-  tabIconDefault: '#ccc',
-  tabIconSelected: tintColor,
+  primaryColor,
+  primaryColorLight,
+  primaryColorDark,
+  primaryColorText,
+  secondaryColor,
+  secondaryColorLight,
+  secondaryColorDark,
+  secondaryColorText,
+  tabIconDefault: '#efefef',
+  tabIconSelected: primaryColor,
   tabBar: '#fefefe',
   errorBackground: 'red',
   errorText: '#fff',
   warningBackground: '#EAEB5E',
   warningText: '#666804',
-  noticeBackground: tintColor,
+  noticeBackground: primaryColor,
   noticeText: '#fff',
 };
