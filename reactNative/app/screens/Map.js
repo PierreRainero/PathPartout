501ba022522dc2ed6628ee90e1aefb6e8acac7ec
Gregory Merlet <gregory.merlet@outlook.fr>
2018-10-18 22:13:27 +0200

    Adding user location and map style to react native app

--- reactNative/app/screens/Map.js
+++ reactNative/app/screens/Map.js
@@ -1,21 +1,73 @@
 import {Component} from "react";
 import {View} from "native-base";
 import React from "react";
-import {StyleSheet} from "react-native";
+import {StyleSheet, Alert} from "react-native";
 import MapView from "react-native-maps";
+import Permissions from 'react-native-permissions';
+import RNAndroidLocationEnabler from 'react-native-android-location-enabler';
 
 export default class Home extends Component<Props> {
+
+    state = {
+        region: {
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.015,
+            longitudeDelta: 0.0121,
+        },
+    };
+
+    componentDidMount() {
+        console.log(this.getLocation());
+        this.getLocation().then(
+            position => {
+                if (position) {
+                    this.setState({
+                        region: {
+                            latitude: position.coords.latitude,
+                            longitude: position.coords.longitude,
+                            latitudeDelta: 0.003,
+                            longitudeDelta: 0.003,
+                        },
+                    });
+                }
+            });
+    }
+
+    async checkLocationEnabled() {
+        return await Permissions.request('location', {
+            rationale: {
+                title: 'Accès à la localisation',
+                message: 'Voulez vous autoriser Path\'Partout à acceder à votre localisation ?',
+            },
+        }).then(response => {
+            if (response != 'authorized')
+                return false;
+            return RNAndroidLocationEnabler.promptForEnableLocationIfNeeded({interval: 10000, fastInterval: 5000})
+                    .then(() => true)
+                    .catch(() => false);
+        });
+    }
+
+    getLocation(){
+        return this.checkLocationEnabled().then((result) => {
+            if(result === true)
+                return new Promise((resolve, reject) => navigator.geolocation.getCurrentPosition(position => resolve(position), e => reject(e)));
+            else
+                Alert.alert('Erreur', 'La localisation n\'est pas activée.');
+        });
+    }
+
     render() {
+        const mapStyle = require('../assets/customMapStyle.json');
         return (
             <View style={styles.container}>
                 <MapView
                     style={styles.map}
-                    region={{
-                        latitude: 37.78825,
-                        longitude: -122.4324,
-                        latitudeDelta: 0.015,
-                        longitudeDelta: 0.0121,
-                    }}
+                    region={this.state.region}
+                    customMapStyle={mapStyle}
+                    showsUserLocation={true}
+                    showsMyLocationButton={true}
                 >
                 </MapView>
             </View>
